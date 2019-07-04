89d22b0955ad3323106126c986c68e218dcb42ca
王雷 <raysaii1991@gmail.com>
2019-07-05 01:17:51 +0800

    1.init

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 const babel = require("rollup-plugin-babel");
 const replace = require("rollup-plugin-replace");
-const commonjs = require("rollup-plugin-commonjs");
-const nodeResolve = require("rollup-plugin-node-resolve");
 const { sizeSnapshot } = require("rollup-plugin-size-snapshot");
 const { uglify } = require("rollup-plugin-uglify");
 const path = require("path");
@@ -51,67 +49,6 @@ const esm = [
   }
 ];
 
-const globals = { react: "React" };
-
-const umd = [
-  {
-    input: "modules/index.js",
-    output: {
-      file: `umd/${pkg.name}.js`,
-      format: "umd",
-      name: "ReactRouterDOM",
-      globals
-    },
-    external: Object.keys(globals),
-    plugins: [
-      babel({
-        exclude: /node_modules/,
-        runtimeHelpers: true,
-        plugins: [["@babel/transform-runtime", { useESModules: true }]]
-      }),
-      nodeResolve(),
-      commonjs({
-        include: /node_modules/,
-        namedExports: {
-          "../react-router/node_modules/react-is/index.js": [
-            "isValidElementType"
-          ]
-        }
-      }),
-      replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
-      sizeSnapshot()
-    ]
-  },
-  {
-    input: "modules/index.js",
-    output: {
-      file: `umd/${pkg.name}.min.js`,
-      format: "umd",
-      name: "ReactRouterDOM",
-      globals
-    },
-    external: Object.keys(globals),
-    plugins: [
-      babel({
-        exclude: /node_modules/,
-        runtimeHelpers: true,
-        plugins: [["@babel/transform-runtime", { useESModules: true }]]
-      }),
-      nodeResolve(),
-      commonjs({
-        include: /node_modules/,
-        namedExports: {
-          "../react-router/node_modules/react-is/index.js": [
-            "isValidElementType"
-          ]
-        }
-      }),
-      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
-      sizeSnapshot(),
-      uglify()
-    ]
-  }
-];
 
 let config;
 switch (process.env.BUILD_ENV) {
@@ -121,11 +58,8 @@ switch (process.env.BUILD_ENV) {
   case "esm":
     config = esm;
     break;
-  case "umd":
-    config = umd;
-    break;
-  default:
-    config = cjs.concat(esm).concat(umd);
+  default :
+    config=esm
 }
 
 module.exports = config;
